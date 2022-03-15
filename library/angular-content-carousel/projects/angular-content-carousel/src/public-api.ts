1b7735d02a10df11485e922c22d6bff31e9aaa69
mojtabanpe <memnpe@gmail.com>
2022-03-15 11:21:06 +0330

    24 esfand

--- library/angular-content-carousel/projects/angular-content-carousel/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of angular-content-carousel
- */
-
-export * from './lib/angular-content-carousel.component';
-export * from './lib/angular-content-carousel.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AngularContentCarouselModule } from 'angular-content-carousel';
 import { FormsModule } from '@angular/forms';
-import { AngularPersianStepperComponent } from './angular-persian-stepper/angular-persian-stepper.component';
+import { AngularPersianStepperModule } from 'angular-persian-stepper';
 
 @NgModule({
   declarations: [
     AppComponent,
     SliderComponent,
-    AngularPersianStepperComponent
+    // AngularPersianStepperComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AngularContentCarouselModule,
-    
+    AngularPersianStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
